12305c7ee65c2811f58f327b198a26ebbc9690a2
Max <max.white.rabbit@gmail.com>
2019-04-25 00:08:39 +0500

    argparse and reader

--- Program.cs
+++ Program.cs
@@ -1,27 +1,75 @@
 ﻿using System;
 using System.IO;
+using System.Collections.Generic;
 
-namespace cs
+
+public class utils
 {
-    class Program
+    static public Dictionary<string, string> argparse(string [] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!11");
-            Console.WriteLine(args);
-        }
-        void reader(string filename)
-        {
-            String line;
-            try
-            {
-                StreamReader fs = new StreamReader(filename);
+        var files = new Dictionary<string, string>();
 
-            }
-            catch(Exception err)
+        if (args.Length == 4)
+        {
+            files.Add(args[0], args[1]);
+            files.Add(args[2], args[3]);           
+        }
+        else
+        {
+            Console.WriteLine("There is no input and output!");
+            return null;
+        }
+
+        return files;
+    }
+    static public string [] reader(string filename)
+    {
+        var lines = new string[] {};
+        try
+        {
+            lines = File.ReadAllLines(filename, System.Text.Encoding.UTF8);
+            foreach(string l in lines)
             {
-                Console.WriteLine(err.Message);
+                Console.WriteLine(l);
+            }
+            if (lines.Length < 2)
+            {
+                throw new System.Exception("Input file has less than two lines!");
             }
         }
+        catch(Exception err)
+        {
+            Console.WriteLine(err.Message);
+        }
+
+        return lines;
     }
 }
+
+class Program
+{
+    static int Main(string[] args)
+    {
+        var files = utils.argparse(args);
+
+        try
+        {
+            Console.WriteLine("Input: {0}", files["-i"]);
+            Console.WriteLine("Output: {0}", files["-o"]);
+
+        }
+        catch(Exception err)
+        {
+            Console.WriteLine("Error: {0}", err.Message);
+            return 1;
+        }
+
+        utils.reader(files["-i"]);
+        
+        
+
+        return 0;
+    }
+
+}
+
